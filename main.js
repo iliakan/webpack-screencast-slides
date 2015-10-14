02ab4a9970e40e33974ef8eff833ed4c240e1ac3
Ilya Kantor <iliakan@gmail.com>
2015-10-14 19:03:52 +0300

    slides

---
+++ main.js
@@ -0,0 +1,95 @@
+function init() {
+  Reveal.initialize({
+    controls: false,
+    progress: false,
+    history: true,
+    center: false, // vertical centering
+    rollingLinks: false,
+
+    transition: 'none', // default/cube/page/concave/zoom/linear/none
+    transitionSpeed: 'fast',
+    backgroundTransition: 'none',  
+
+    // Optional libraries used to extend on reveal.js
+    dependencies: [
+
+      {
+        src: 'node_modules/reveal.js/plugin/markdown/marked.js',
+        condition: function() {
+          return !!document.querySelector('[data-markdown]');
+        }
+      }, {
+        src: 'node_modules/reveal.js/plugin/markdown/markdown.js',
+        condition: function() {
+          return !!document.querySelector('[data-markdown]');
+        }
+      }, {
+        src: 'node_modules/reveal.js/plugin/highlight/highlight.js',
+        async: true,
+        callback: function() {
+          hljs.initHighlightingOnLoad();
+        }
+      }, {
+        src: 'node_modules/reveal.js/plugin/zoom-js/zoom.js',
+        async: true,
+        condition: function() {
+          return !!document.body.classList;
+        }
+      }, {
+        src: 'node_modules/reveal.js/plugin/notes/notes.js',
+        async: true,
+        condition: function() {
+          return !!document.body.classList;
+        }
+      }
+    ]
+  });
+
+  document.addEventListener("fragmentshown", function(e) {
+    setCurrentFragment(e.fragment)
+  }, false);
+  document.addEventListener("fragmenthidden", function(e) {
+    setCurrentFragment(e.fragment)
+  }, false);
+
+  Array.from(document.querySelectorAll('.fragments > li')).forEach(
+    elem => elem.classList.add('fragment')
+  );
+  var fragments = document.querySelector('.fragments');
+
+  if (fragments) {
+    fragments.insertAdjacentHTML("beforeEnd", '<li class="fragment" hidden></li>');
+  }
+}
+
+
+function setCurrentFragment(fragment) {
+
+  var section = fragment.closest('section');
+
+  var fragmentCurrent = section.querySelector('.fragment-current');
+
+  if (fragmentCurrent) {
+    fragmentCurrent.classList.remove('fragment-current');
+  }
+
+  var visible = section.querySelectorAll('.visible');
+  if (visible.length) {
+    visible[visible.length - 1].classList.add('fragment-current');
+  }
+}
+
+init();
+
+/*
+var path = document.location.pathname + "index.html";
+$.get(path)
+  .success(function(content) {
+    content = content.replace(/`(.*?)`/gim, '<code>$1</code>');
+    $('#main').html(content);
+    setTimeout(init, 0);
+  })
+  .fail(function(err) {
+    alert(err);
+  })*/
+
